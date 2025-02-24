bfa02520f75ebb652c5dc9e7e94ebce5447b4711
VandanaMahadev <vandanamahadevvandu@gmail.com>
2025-02-24 18:20:14 +0530

    new file modifed

---
+++ ofrs/ofrs/files.php
@@ -0,0 +1,50 @@
+<!-- Content Row-->
+<div class="row gx-4 gx-lg-5">
+                <div class="col-md-12 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+
+
+
+     
+  <div class="row">
+
+                        <div class="col-lg-6">
+
+
+                        <!-- Content Row-->
+<div class="row gx-4 gx-lg-5">
+                <div class="col-md-12 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+
+
+
+     
+  <div class="row">
+
+                        <div class="col-lg-6">
+                            <!-- Content Row-->
+<div class="row gx-4 gx-lg-5">
+                <div class="col-md-12 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+
+
+
+     
+  <div class="row">
+
+                        <div class="col-lg-6">
+                            <!-- Content Row-->
+<div class="row gx-4 gx-lg-5">
+                <div class="col-md-12 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+
+
+
+     
+  <div class="row">
+
+                        <div class="col-lg-6">
